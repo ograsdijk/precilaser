24e2c593eb6bd6128dfcda6f51edd28d52e7ac6e
ograsdijk <o.grasdijk@gmail.com>
2023-01-11 17:07:30 -0600

    update example

--- examples/seed_status.py
+++ examples/seed_status.py
@@ -7,3 +7,12 @@ dev = Seed(resource_name=resource_name, addresss=100)
 print(dev.status)
 print(dev.temperature_setpoint)
 print(dev.piezo_voltage)
+
+temperature_setpoint = dev.temperature_setpoint
+
+print(f"temperature setpoint is {temperature_setpoint}")
+dev.temperature_setpoint = temperature_setpoint + 1
+print(
+    f"changed temperature setpoint to {temperature_setpoint+1}, device temperature"
+    f" setpoint is {dev.temperature_setpoint}"
+)
